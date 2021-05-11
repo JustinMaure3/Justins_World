26e7395fd1e7902bdd5a9239af0e4eb3546c5738
Justin Maure <32267749+JustinMaure3@users.noreply.github.com>
2021-05-11 12:33:56 -0400

    Added the Main Menu
    #10 I created the main menu and added functionality for the start and quit buttons

---
+++ JustinWorld/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+    //Function that will start the first level
+    public void OnStart() {
+        SceneManager.LoadScene("Level1", LoadSceneMode.Single);
+    }
+
+    //Function that will bring the user to the options screen
+    public void OnOptions() {
+
+    }
+
+    //Function that will exit the application
+    public void OnQuit() {
+        Application.Quit();
+        print("It quit");
+    }
+
+}
