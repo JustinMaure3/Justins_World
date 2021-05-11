576257eaffbd319cfaddb7d7231bd3d01f3ac99a
Justin Maure <32267749+JustinMaure3@users.noreply.github.com>
2021-05-10 20:20:34 -0400

    Added the Green Frog
    #7 Added a new enemy that will push the player. I added all animations that went with the whirlwind and frog as well as a few scripts to go with them.

---
+++ JustinWorld/Assets/Scripts/Enemies/GreenFrogController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenFrogController : MonoBehaviour {
+
+    private Animator anim;
+    public float direction;
+
+    public Transform whirlwind;
+
+    //This is used to have a certain distance before the frog turns towards the player
+    public float turnDistance = 0.3f;
+
+    private void Awake() {
+        anim = GetComponent<Animator>();
+    }
+
+    //Runs every frame
+    private void FixedUpdate() {
+        SetDirection();
+    }
+
+    //This will be used to set the x value in animator depending on where the player is
+    private void SetDirection() {
+
+        Vector3 playerPos = GameObject.FindWithTag("Player").transform.position;
+
+        direction = 0;
+
+        if (playerPos.x > transform.position.x + turnDistance) {
+            direction += 1;
+        } else if (playerPos.x < transform.position.x - turnDistance) {
+            direction -= 1;
+        }
+
+        whirlwind.localPosition = new Vector3(0.6f * direction, whirlwind.localPosition.y, whirlwind.localPosition.z);
+        SetAnim();
+    }
+
+    private void SetAnim() {
+        if (direction != 0) {
+            anim.SetFloat("x", direction);
+        }
+    }
+
+
+}
---
+++ JustinWorld/Assets/Scripts/Items/Whirlwind.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whirlwind : MonoBehaviour {
+
+    //Amount of force that will push player
+    public float windForce = 50f;
+    public float windLength = 0.5f;
+
+    private Transform playerPos;
+    private float direction = 0f;
+
+    private Animator anim;
+
+    //Awake method
+    private void Awake() {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnEnable() {
+        playerPos = GameObject.FindWithTag("Player").transform;
+
+        //Get direction
+        direction = this.GetComponentInParent<GreenFrogController>().direction;
+        anim.SetFloat("x", direction);
+    }
+
+    //On trigger this will start the push player coroutine
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.tag == "Player") {
+            StartCoroutine(PushPlayer(collision));
+
+        }
+    }
+
+    //This will apply force to the player
+    IEnumerator PushPlayer(Collider2D collision) {
+        collision.GetComponent<CharacterMovement>().DisableMovement();
+
+        Rigidbody2D player = collision.GetComponent<Rigidbody2D>();
+        player.AddForce(new Vector2(direction * windForce, 0f), ForceMode2D.Impulse);
+
+        yield return new WaitForSeconds(windLength);
+
+        collision.GetComponent<CharacterMovement>().EnableMovement();
+    }
+}
